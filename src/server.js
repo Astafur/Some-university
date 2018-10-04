70c1167726cbd9c76a03975208565b87ae9b669d
Pavel <89089079540@mail.ru>
2018-10-04 14:54:24 +0500

    Delete server.js

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express        = require('react');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const app            = react();
-
-const port = 3000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
